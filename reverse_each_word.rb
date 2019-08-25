2516bde78e2a791bd0748278b644ce9148e7f9e0
Marc Blanchard <marcblanchard0930@gmail.com>
2019-08-25 01:07:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(string)
-  array = string.split
-  # test_array = []
+  array = string.split(" ")
+   test_array = []
   
   array.collect do |word| 
-    word.reverse 
+    test_array << word.reverse 
   end
   array.join(" ")
 end
